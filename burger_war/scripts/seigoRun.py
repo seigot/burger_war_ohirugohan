adf19e6eb9757199a0b3cf6a70bc741e32e3e89f
seigot <s.takada.3o3@gmail.com>
2019-07-27 12:20:19 +0900

    update seigoRun.py, add image/laserscan basic function.

--- burger_war/scripts/seigoRun.py
+++ burger_war/scripts/seigoRun.py
@@ -3,21 +3,50 @@
 import rospy
 import random
 
+from std_msgs.msg import String
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
 
-#from std_msgs.msg import String
-#from sensor_msgs.msg import Image
-#from cv_bridge import CvBridge, CvBridgeError
-#import cv2
 
-
-class RandomBot():
+class SeigoBot():
     def __init__(self, bot_name):
         # bot name 
         self.name = bot_name
         # velocity publisher
         self.vel_pub = rospy.Publisher('cmd_vel', Twist,queue_size=1)
 
+        # Lidar
+        self.scan = LaserScan()
+        self.lidar_sub = rospy.Subscriber('/red_bot/scan', LaserScan, self.lidarCallback)
+        
+        # usb camera
+        self.img = None
+        self.camera_preview = True
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber('/red_bot/image_raw', Image, self.imageCallback)
+
+    # lidar scan topic call back sample
+    # update lidar scan state
+    def lidarCallback(self, data):
+        self.scan = data
+        print(self.scan)
+                    
+    # camera image call back sample
+    # comvert image topic to opencv object and show
+    def imageCallback(self, data):
+        try:
+            self.img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+        #    if self.camera_preview:
+        print("image show")
+        cv2.imshow("Image window", self.img)
+        cv2.waitKey(1)
+          
     def calcTwist(self):
         value = random.randint(1,1000)
         if value < 250:
@@ -42,12 +71,13 @@ class RandomBot():
 
     def strategy(self):
         r = rospy.Rate(1) # change speed 1fps
-
+        
         target_speed = 0
         target_turn = 0
         control_speed = 0
         control_turn = 0
 
+        # Main Loop --->
         while not rospy.is_shutdown():
             twist = self.calcTwist()
             print(twist)
@@ -55,9 +85,10 @@ class RandomBot():
 
             r.sleep()
 
-
+        # Main Loop <---
+        
 if __name__ == '__main__':
-    rospy.init_node('random_run')
-    bot = RandomBot('Random')
+    rospy.init_node('seigo_run')
+    bot = SeigoBot('Seigo')
     bot.strategy()
 
