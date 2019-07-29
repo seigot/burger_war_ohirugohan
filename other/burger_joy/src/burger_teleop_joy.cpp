de3bb5f70ad0a5d0fd7048ad3ce052226fa225c6
seigot <s.takada.3o3@gmail.com>
2019-07-30 00:53:42 +0900

    add burger_joy (teleop key + teleop joy)

--- other/burger_joy/src/burger_teleop_joy.cpp
+++ other/burger_joy/src/burger_teleop_joy.cpp
@@ -9,13 +9,16 @@ public:
   TeleopTurtle();
 
 private:
+  void TurtleCallback(const geometry_msgs::Twist::ConstPtr& _twist);
   void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 
   ros::NodeHandle nh_;
 
   int linear_, angular_;
   double l_scale_, a_scale_;
-  ros::Publisher vel_pub_;
+  ros::Publisher vel_pub_red_;
+  ros::Publisher vel_pub_blue_;
+  ros::Subscriber turtle_sub_;
   ros::Subscriber joy_sub_;
 
 };
@@ -33,10 +36,20 @@ TeleopTurtle::TeleopTurtle():
 
 
   //vel_pub_ = nh_.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
-  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("red_bot/cmd_vel", 1);
-
+  vel_pub_red_ = nh_.advertise<geometry_msgs::Twist>("red_bot/cmd_vel", 1);
+  vel_pub_blue_ = nh_.advertise<geometry_msgs::Twist>("blue_bot/cmd_vel", 1);
+  
+  turtle_sub_ = nh_.subscribe<geometry_msgs::Twist>("turtle1/cmd_vel", 10, &TeleopTurtle::TurtleCallback, this);
   joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopTurtle::joyCallback, this);
 
+}
+
+void TeleopTurtle::TurtleCallback(const geometry_msgs::Twist::ConstPtr& _twist)
+{
+  geometry_msgs::Twist twist;
+  twist.angular.z = 0.35*_twist->angular.z;
+  twist.linear.x = 0.25*_twist->linear.x;
+  vel_pub_red_.publish(twist);
 }
 
 void TeleopTurtle::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
@@ -44,7 +57,7 @@ void TeleopTurtle::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
   geometry_msgs::Twist twist;
   twist.angular.z = a_scale_*joy->axes[angular_];
   twist.linear.x = l_scale_*joy->axes[linear_];
-  vel_pub_.publish(twist);
+  vel_pub_blue_.publish(twist);
 }
 
 
