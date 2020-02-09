ab5c65a1ad5aca55388c6e09969d4897de9b27ca
seigot <s.takada.3o3@gmail.com>
2020-02-10 02:14:17 +0900

    for open cv trial

--- burger_war/scripts/seigoRun.py
+++ burger_war/scripts/seigoRun.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''
-This is rumdom run node.
-subscribe No topcs.
-Publish 'cmd_vel' topic. 
-mainly use for simple sample program
-
-by Takuya Yamaguhi.
-'''
 
 import rospy
 import random
 
+from geometry_msgs.msg import PoseWithCovarianceStamped
+import tf
+from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+import numpy as np
+import time
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from nav_msgs.msg import Odometry
+import actionlib_msgs
+
 from geometry_msgs.msg import Twist
 
+# camera image 640*480
+img_w = 640
+img_h = 480
+image_resize_scale = 1 # 8
+
+# PI
+PI = 3.1415
 
 class RandomBot():
     def __init__(self, bot_name="NoName"):
@@ -21,8 +33,76 @@ class RandomBot():
         self.name = bot_name
         # velocity publisher
         self.vel_pub = rospy.Publisher('cmd_vel', Twist,queue_size=1)
+        # navigation publisher
+        self.client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
+
+        # usb camera
+        self.img = None
+        self.camera_preview = True
+        self.bridge = CvBridge()
+        topicname_image_raw = "image_raw"
+        self.image_sub = rospy.Subscriber(topicname_image_raw, Image, self.imageCallback)
+
+    # camera image call back sample
+    # convert image topic to opencv object and show
+    def imageCallback(self, data):
+        try:
+            self.img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+        size = (img_w/image_resize_scale, img_h/image_resize_scale)
+        frame = cv2.resize(self.img, size)
+        if self.camera_preview:
+            print("image show")
+            cv2.imshow("Image window", frame)
+            cv2.waitKey(1)
+
+    # Ref: https://hotblackrobotics.github.io/en/blog/2018/01/29/action-client-py/
+    # Ref: https://github.com/hotic06/burger_war/blob/master/burger_war/scripts/navirun.py
+    # RESPECT @hotic06
+    # do following command first.
+    #   $ roslaunch burger_navigation multi_robot_navigation_run.launch
+    def setGoal(self,x,y,yaw):
+        self.client.wait_for_server()
+
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = self.name + "/map"
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x = x
+        goal.target_pose.pose.position.y = y
+
+        # Euler to Quartanion
+        q=tf.transformations.quaternion_from_euler(0,0,yaw)        
+        goal.target_pose.pose.orientation.x = q[0]
+        goal.target_pose.pose.orientation.y = q[1]
+        goal.target_pose.pose.orientation.z = q[2]
+        goal.target_pose.pose.orientation.w = q[3]
+
+        self.client.send_goal(goal)
+        wait = self.client.wait_for_result()
+        if not wait:
+            rospy.logerr("Action server not available!")
+            rospy.signal_shutdown("Action server not available!")
+            return -1
+
+        get_state = self.client.get_state()
+        print("wait", wait, "get_state", get_state)
+        if get_state == 2:  # if send_goal is canceled
+            return -1
+
+        return 0
+
+    def cancelGoal(self):
+        self.client.cancel_goal()
 
     def calcTwist(self):
+
+        _x = -0.4
+        _y = 0.0
+        _th = 0
+        # ret = self.setGoal(_x, _y, _th)
+
         value = random.randint(1,1000)
         if value < 250:
             x = 0.2
