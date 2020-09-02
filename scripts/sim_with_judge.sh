a5e54fd6997b771e6ee9ef9b9f9ee7c54780264a
seigot <you@example.com>
2020-09-02 15:05:51 +0000

    for debug, update to work on blue side.
    in detail, do following to work on blue side.
    
    bash script/sim_with_judge.sh -s b
    bash script/start.sh -l 3 -s b
    
    RESPECT hotic06

--- scripts/sim_with_judge.sh
+++ scripts/sim_with_judge.sh
@@ -2,14 +2,36 @@
 set -e
 set -x
 
+# set default your color red
+VALUE_SIDE="r"
+
+# get args level setting
+while getopts s: OPT
+do
+  case $OPT in
+    "s" ) FLG_S="TUR"  ; VALUE_SIDE="$OPTARG" ;;
+  esac
+done
+
+
 # judge
 # run judge server and visualize window
 gnome-terminal -e "python judge/judgeServer.py"
+sleep 1
 gnome-terminal -e "python judge/visualizeWindow.py"
 
 # init judge server for sim setting
-bash judge/test_scripts/init_single_play.sh judge/marker_set/sim.csv localhost:5000 you enemy
+case $VALUE_SIDE in
+    "r" ) bash judge/test_scripts/init_single_play.sh judge/marker_set/sim.csv localhost:5000 you enemy ;;
+    "b" ) bash judge/test_scripts/init_single_play.sh judge/marker_set/sim.csv localhost:5000 enemy you ;;
+    * ) bash judge/test_scripts/init_single_play.sh judge/marker_set/sim.csv localhost:5000 you enemy ;;
+esac
+
 
 # robot
-roslaunch burger_war setup_sim.launch
+case $VALUE_SIDE in
+    "r" ) roslaunch burger_war setup_sim.launch ;;
+    "b" ) roslaunch burger_war setup_sim_blue.launch ;;
+    * ) roslaunch burger_war setup_sim.launch ;;
+esac
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/bin/bash -uex
 
 # set default level 1
 VALUE_L="1"
-
+# set default color red
+VALUE_SIDE="r"
 
 # get args level setting
-while getopts l: OPT
+while getopts l:s: OPT
 do
   case $OPT in
-    "l" ) FLG_L="TRUE" ; VALUE_L="$OPTARG" ;;
+    "l") FLG_L="TRUE" ; VALUE_L="$OPTARG" ;;
+    "s") FLG_S="TRUE" ;  VALUE_SIDE="$OPTARG" ;;
   esac
 done
 
@@ -16,4 +18,4 @@ done
 bash judge/test_scripts/set_running.sh localhost:5000
 
 # launch robot control node
-roslaunch burger_war sim_robot_run.launch enemy_level:=$VALUE_L
+roslaunch burger_war sim_robot_run.launch enemy_level:=$VALUE_L side:=$VALUE_SIDE
